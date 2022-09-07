cb48e3aa788710a2f60cb48ecfb3cf75bf34f636
MusicShaun <music.shaunpickett@gmail.com>
2022-09-07 10:33:41 +0800

    Huge overhaul. Not sure if this version is better than my other version.
    More extensive and less concise. Easier to read though.
    No one seems to be able to figure out how to prevent double letters highlighting

---
+++ src/components/Keyboard.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useCallback, useContext } from 'react'
+import styled from 'styled-components';
+import Key from './Key';
+import {AppContext} from '../App';
+import { fSize } from '../fonts&screen';
+
+function Keyboard() {
+  const { handleDelete, handleEnter, handleLetterAdd , disabledLetters} = useContext(AppContext);
+  const keys1 = ['q','w','e','r','t','y','u','i','o','p'];
+  const keys2 = ['a','s','d','f','g','h','j','k','l'];
+  const keys3 = ['z','x','c','v','b','n','m'];
+
+  // eslint-disable-next-line 
+ const handleKeyboard = useCallback((event) => {
+  if (event.key === 'Enter') {
+    handleEnter();
+  } else if (event.key === 'Backspace') {
+    handleDelete();
+
+  } else {
+    keys1.forEach((key) => {
+      if (event.key.toLowerCase() === key) {
+        handleLetterAdd(key)
+      }
+    })
+    keys2.forEach((key) => {
+      if (event.key.toLowerCase() === key) {
+        handleLetterAdd(key)
+      }
+    })
+    keys3.forEach((key) => {
+      if (event.key.toLowerCase() === key) {
+        handleLetterAdd(key)
+      }
+    })
+  }
+ })
+
+ useEffect(() => {
+  document.addEventListener('keydown', handleKeyboard);
+  return () => document.removeEventListener('keydown', handleKeyboard);
+}, [handleKeyboard])
+
+
+
+  return (
+    <Wrapper onKeyDown={handleKeyboard}>
+      <Keys>
+        <Keyrow>
+          {keys1.map((item, index) => {
+            return <Key key={index} keyVal={item} disabled={disabledLetters.includes(item)}/>
+          })}
+        </Keyrow>
+        <Keyrow>
+          {keys2.map((item, index) => {
+            return <Key key={index+12}  keyVal={item} disabled={disabledLetters.includes(item)}/>
+          })}
+        </Keyrow>
+        <Keyrow>
+          <Key keyVal={'ENT'} bigKey/>
+          {keys3.map((item, index) => {
+            return <Key  key={index + 24}  keyVal={item} disabled={disabledLetters.includes(item)}/>
+          })}
+          <Key keyVal={'DEL'} bigKey/>
+        </Keyrow>
+      </Keys>
+    </Wrapper>
+  )
+}
+
+export default Keyboard
+
+
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 96%;
+  height: auto;
+  padding-bottom: 2rem;
+  ${fSize.laptop}{
+    max-width: 600px;
+  }
+`;
+const Keys = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  ${fSize.large};
+  width: 98%;
+`;
+const Keyrow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+`;
+
